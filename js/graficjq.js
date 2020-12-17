434a7a548e5596580bf29cb58cb083ec42a6c569
alexey <kondakov@proitr.ru>
2020-12-17 10:54:21 +0500

    Выпилил все лишнее

--- js/graficjq.js
+++ js/graficjq.js
@@ -8,84 +8,42 @@ var init = function() {
         {
             countDiagrams,
             maxValue,
-            dashValues: [10, 15, 34, 48, 24]
+            dashValues: [480, 450, 430, 400, 380]
         }
     );
     drawChart.draw();
 };
 
 var DrawChart = function(options){
+
+    this.count = parseInt(options.countDiagrams)+1;
+    this.width = parseInt(options.maxValue);
+    this.dashValues = options.dashValues;
+
     this.draw = function() {
         var histogram = document.getElementsByClassName("histogram")[0];
-        
-
+        // Чистим поле диаграмм
         histogram.innerHTML='';
-        
-        var control = document.createElement("div");
-        var formControl = document.createElement("form");
-        control.append(formControl);
-        
-        let count = parseInt(options.countDiagrams)+1;
-        let width = parseInt(options.maxValue);
-
-        // histogram.css('histogram', width+'px');
-        $('.histogram').css('width', width+'px' );
-
-        for(var i = 1; i < count; i++) {
-            let itemControl = document.createElement("div");
-            itemControl.innerHTML = "<label>"+i+": </label>";
-            let inputItem = document.createElement("input");
-            inputItem.setAttribute("type", "text");
-            inputItem.setAttribute("class", "reviews_"+i+"star");
-            itemControl.append(inputItem);
-            formControl.append(itemControl);
-        }        
-        histogram.append(control);
-        for(var i = 1; i < count; i++) {
+        // Задаем ширину для диаграмм
+        $('.histogram').css('width', this.width+'px' );        
+        // Рисуем поля для графиков и метки
+        for(var i = 1; i < this.count; i++) {
             let containerBar = document.createElement("div");
             containerBar.setAttribute("class", "bar-container");
             containerBar.setAttribute("data-id", i);
-            containerBar.innerHTML = '<span class="bar-label"> '+i+' </span><span class="bar"><span class="dash"></span></span><span class="bar-number"></span>';
-            let ml = parseInt(options.dashValues[i-1]);
+            containerBar.innerHTML = '<span class="bar-label"> '+i+' </span><span class="bar"><span class="dash"></span></span><span class="bar-number"></span>';                       
+            histogram.append(containerBar);
+            let ml = parseInt(this.dashValues[i-1]);
             if (ml) {
                 $('.bar-container[data-id='+(i)+'] .dash').css('margin-left', ml+'px' );
-            }            
-            histogram.append(containerBar);
+            } 
         }
     };
-    this.start = function() {
-        var stars = new Array();
-        var sum = 0;
-        var width = new Array();
 
-        let count = parseInt(options.countDiagrams)+1;
-        let maxValue = parseInt(options.maxValue);
-        
-        // Считываем значения
-        for ( var i = 1; i < count; i++ ) {
-            // stars.push(parseInt($('.reviews_'+i+'star').val()));
-            stars.push(maxValue);
-        }     
-        // Считаем сумму 
-        for ( var i = 0; i < stars.length; i++ ) {
-            sum += stars[i];       
-        }     
-        // Расчитываем проценты для графика
-        // Выставляем ширину в процентах
-        sum = maxValue; 
-        for ( var i = 0; i < stars.length; i++ ) {
-            w = ((stars[i]) / sum * 100).toFixed(0);        
-            width.push(w);
-            $('.bar-container[data-id='+(i+1)+'] .bar').css('width', maxValue+'px' );
+    this.start = function() {
+        // Рисуем бары
+        for ( var i = 0; i < this.count; i++ ) {            
+            $('.bar-container[data-id='+(i+1)+'] .bar').css('width', this.width+'px' );              
         }
-        // Подставляем значения в лейблы
-        // if (sum > 0) {
-        // for ( var i = 0; i < stars.length; i++ ) {
-        //     // $('.bar-container[data-id='+(i+1)+'] .bar-number').html(width[i]+'%'); 
-        //     $('.bar-container[data-id='+(i+1)+'] .bar-number').html(width[i]); 
-        // }
-        // } else{
-        // $(".bar-container .bar-number").html('0%')
-        // }
     };
 };
